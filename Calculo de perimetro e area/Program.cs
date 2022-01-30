1376af24322e3ee2ed45cb85ddf00af7dbdd490a
Guilherme Miguel <guilherme.souza1996@hotmail.com>
2022-01-30 14:45:39 -0300

    Exercicio de calculo de retangulo

---
+++ Calculo de perimetro e area/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculo_de_perimetro_e_area
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao programa de calculo de area e perimetro de retangulo");
+
+            Console.WriteLine("Digite a largura");
+            var width = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura");
+            var height = float.Parse(Console.ReadLine());
+
+            var area = CalculateArea(width, height);
+            var perimeter = CalculatePerimeter(width, height);
+
+            Console.WriteLine($"A area do retangulo é: {area}");
+            Console.WriteLine($"O perimetro do retangulo é: {perimeter}");
+
+            Console.ReadLine();
+        }
+
+        private static float CalculateArea(float width, float height) =>
+            width * height;
+
+        private static float CalculatePerimeter(float width, float height) =>        
+            (width * 2) + (height * 2);
+        
+    }
+}
